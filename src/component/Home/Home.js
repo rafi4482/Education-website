87678e7ff01b616ebb6d9a081343cd2162c67978
rafi4482 <rafi.kazla@gmail.com>
2021-10-04 10:23:56 +0600

    ba

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,86 @@
-import React from 'react';
+import "./Home.css"
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from "react-bootstrap";
 
 const Home = () => {
+
+  const[card,setCard]=useState([]);
+
+useEffect(()=>{
+fetch('./serviceprovide.JSON')
+.then(res=>res.json())
+.then(data=>setCard(data))
+},[])
+
+
     return (
-        <div>
-          <h1>this is home compo</h1>  
+      <div>
+
+        <div className="cont">
+          <h1>welcome to Rafi Education</h1>  
+          <p>we provide the best quality services</p>        
         </div>
+
+
+        <div className="card-create">
+
+          <div className="row">
+
+
+            {
+              card.map(c=><div className="col-md-3">
+                <div className="card-create1">
+
+                  <div className="image">
+                    <img className="w-50 mx-5" src={c.img} alt="" />
+                  </div>
+                  <div>
+                    <h3>Course Name: {c.name}</h3>
+                    <h4>Course Fee: {c.price}</h4>
+                    <p>{c.desc}</p>
+                  
+
+                  </div>
+                </div>
+                </div>
+                )
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+          </div>
+
+
+
+
+        </div>
+        
+    
+   
+      
+
+          
+        
+   
+        <footer>
+        <p>footer</p>
+        </footer>
+      </div>
+        
     );
 };
 
---
+++ src/fakedata/serviceprovide.js
@@ -0,0 +1,28 @@
+const serviceprovide =[
+
+{
+  name:'Graphics Designing',
+  price: 3000,
+  desc: 'This is a professional graphics design course',
+  img:'https://cosmosgroup.sgp1.cdn.digitaloceanspaces.com/thumbnail/lead/news/1071218_Graphic-Design.jpg',
+},
+{
+    name:'Android App Developement',
+    price: 4000,
+    desc: 'This course provides a complete walkthrough of professional android app developement',
+    img:'https://i0.wp.com/opensourceforu.com/wp-content/uploads/2018/05/Android-app-development.jpg?fit=1987%2C1088&ssl=1',
+  },
+  {
+    name:'Digital Marketing',
+    price: 3500,
+    desc: 'This course is designed to make sure you would be an expert digital marketer',
+    img:'https://neilpatel.com/wp-content/uploads/2021/07/digital-marketing_featured-image.png',
+  },
+  {
+    name:'Data Entry',
+    price: 4500,
+    desc: 'Simple and convenient approach to be a beginner freelancer',
+    img:'https://static1.shine.com/l/m/images/blog/data-entry-jobs.jpg',
+  }
+
+]
