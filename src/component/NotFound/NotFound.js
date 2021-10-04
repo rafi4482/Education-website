928772da84f7e4054b5be2e6777232521da0f1d1
rafi4482 <rafi.kazla@gmail.com>
2021-10-04 21:11:24 +0600

    back2222222222222

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -4,10 +4,10 @@ import Footer from '../Footer/Footer';
 const NotFound = () => {
     return (
         <div>
-            <h1>error 444 page not found</h1>
+            <h1>error 404 page not found</h1>
             <Footer></Footer>
         </div>
     );
 };
 
-export default NotFound;<h1>error 444 page not found</h1>
+export default NotFound;<h1>error 404 page not found</h1>
