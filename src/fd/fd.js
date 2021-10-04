cd2f3b4150ac683653f9b51043d4e7f9eb23954c
rafi4482 <rafi.kazla@gmail.com>
2021-10-04 15:49:04 +0600

    back22

---
+++ src/fd/fd.js
@@ -0,0 +1,57 @@
+const serviceprovide =[
+
+    {
+      name:'Python crash course',
+      price: 3000,
+      desc: 'This is a professional Python course',
+      img:'https://www.gom.com/-/media/gom-website/global/services/gom-training-overview/elearning/gom_elearning-python-for-beginners_teaser.jpg?as=0&dmc=0&thn=0',
+    },
+    {
+        name:'Java crash course',
+        price: 4000,
+        desc: 'This course provides a complete walkthrough of professional Java developement',
+        img:'https://www.gcreddy.com/2021/07/java-tutorial.html',
+      },
+      {
+        name:'C crash course',
+        price: 3500,
+        desc: 'This course is designed for absolute beginners',
+        img:'https://latx-technologies.com/wp-content/uploads/2021/03/c-programming-course-1024x553.jpg',
+      },
+      {
+        name:'C++ crash course',
+        price: 4500,
+        desc: 'From beginning to advanced C++ course',
+        img:'https://bs-uploads.toptal.io/blackfish-uploads/blog/article/content/cover_image_file/cover_image/13650/cover-0828_AfterAllTheseYearstheWorldisStillPoweredbyCProgramming_Razvan_Newsletter-2b9ea38294bb08c5aea1f0c1cb06732f.png',
+      },
+      {
+        name:'C# crash course',
+        price: 4200,
+        desc: 'C# for delevopement reasons',
+        img:'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Csharp.png',
+      },
+      {
+        name:'Wordpress crash course',
+        price: 4300,
+        desc: 'Advanced customization possible',
+        img:'https://s.w.org/about/images/logos/wordpress-logo-stacked-rgb.png',
+      },
+      {
+        name:'Matlab crash course',
+        price: 4400,
+        desc: 'This course is designed for university requirements',
+        img:'https://www.marinetraining.eu/sites/default/files/styles/course_view/public/course/Matlab-Logo.png?itok=VGTH9xL0',
+      },
+      {
+        name:'Angular crash course',
+        price: 5500,
+        desc: 'A Javascript framework used worldwide',
+        img:'https://miro.medium.com/max/480/1*9A6E9kaZZ54idy0HLSlh-A.png',
+      },
+      {
+        name:'Django crash course',
+        price: 5600,
+        desc: 'Python framework used in various reasons',
+        img:'https://www.fullstackpython.com/img/logos/django.png',
+      }
+    ]
