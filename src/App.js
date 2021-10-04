e523a382459453bdad4f5f62af00391338e9b64b
rafi4482 <rafi.kazla@gmail.com>
2021-10-04 07:14:28 +0600

    s

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { ButtonGroup,Button} from 'react-bootstrap';
+import Home from './component/Home/Home';
+import About from './component/About/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NotFound from './component/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+     <Router>
+     <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route exact path="/home">
+          <Home></Home>
+        </Route>
+        <Route exact path="/about">
+          <About></About>
+        </Route>
+        <Route exact path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+     </Router>
      
-      <Button>hello</Button>
     </div>
   );
 }
